6939e4e691ef63f044e0d0c7cf17392211daf0fe
Islan <islan12victhor@gmail.com>
2024-07-20 21:46:31 -0300

    Implementação de Conversor de Base Decimal-N

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 import { Stack } from "./estruturas.js";
+import { baseConverter } from "./algoritmos.js";
 
-const stack = new Stack()
-stack.push(1)
-stack.push(5)
-stack.push(51)
-stack.push(17)
-stack.push(22)
-stack.push(13)
-console.log(stack.pop())
-console.log(stack.peek())
-console.log(stack.toString())
+//  ALGORITMOS  -   CONVERSOR BASE DECIMAL-N
+$('#conversor-article div').empty()
+$('#conversor-article div').append(`
+    <ul class="list-disc list-inside text-sm py-2 bg-sky-200">
+        <li>100345 -> base2 = <strong>${baseConverter(100345)}</strong></li>
+        <li>100345 -> base8 = <strong>${baseConverter(100345, 8)}</strong></li>
+        <li>100345 -> base16 = <strong>${baseConverter(100345, 16)}</strong></li>
+        <li>100345 -> base35 = <strong>${baseConverter(100345, 35)}</strong></li>
+        <li><input type="number" id="conversor-article-number" min="0" class="max-w-20 bg-orange-100 pl-1"> -> <input type="number" id="conversor-article-base" min="2" max="36" class="bg-orange-100 pl-1"> = <strong><span id="conversor-article-span">???</span></strong> <input type="button" id="conversor-article-button" value="Aleluia Glória a Deus" class="bg-orange-200 active:bg-orange-100 px-2 font-bold"></li>
+    </ul>
+`)
+$('#conversor-article-button').on('click', function (event) {
+    event.preventDefault()
+    const number = $('#conversor-article-number').val()
+    const base = $('#conversor-article-base').val()
+
+    let convertido = baseConverter(number, base)
+    convertido = convertido ? convertido : '???'
+    
+    $('#conversor-article-span').empty()
+    $('#conversor-article-span').append(convertido)
+})
